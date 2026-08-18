4b48dc765fc46449d9a410a8f70ebbfaf5912498
Debabrata24 <mr.debabrtapc2006@gmail.com>
2026-08-18 12:27:31 +0530

    rust

--- Rusts/main.rs
+++ Rusts/main.rs
@@ -1,16 +1,38 @@
-fn main() {
-    // Define the name variable
-    let name = "Alice";
+// fn main() {
+//     // Define the name variable
+//     // let name = "Alice";
 
-    // Print the name using a placeholder
-    println!("Hello, {}!", name);
-    let a =10;
-   let b =40;
-   let ans = add_number(a,b);
-   println!("{}",ans);
+//     // Print the name using a placeholder
+//     // println!("Hello, {}!", name);
+//    let str = String::from("Hello");
+//     finder (str);
+//     let a =10;
+//    let b =40;
+//    let ans = add_number(a,b);
+//    println!("{}",ans);
+//     // println!("{}",str);
+//     println!("{}",a);
+// }
+
+// fn add_number(x:i32 ,y:i32)->i32{
+//     return  x +y;
+//     // println!("{}",x+y);
+// }
+// fn finder (s:String){
+//     println!("{}",s);
+
+//     //drop
+// }
+
+
+fn main (){
+    let str  = String::from ("Strike is commig");
+    // only read me 
+   immutable_borrow(&str);
+println!("{}", str)
+}
+fn immutable_borrow (s:&String){
+ println!("{}",s)       
 }
 
-fn add_number(x:i32 ,y:i32)->i32{
-    return  x +y;
-    // println!("{}",x+y);
-}
+// 40
