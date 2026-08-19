5fa401a79a848f4d33f8e8f08e5458f5ee733a11
Debabrata24 <mr.debabrtapc2006@gmail.com>
2026-08-20 01:10:07 +0530

    rust_5

---
+++ Rusts/main5.rs
@@ -0,0 +1,14 @@
+ 
+
+fn main(){
+
+    let name = String::from("How to wins friends and influensers");
+
+    let author =  String::from("dale careneggie");
+
+    let page_count =120;
+
+    let price = 400;
+    
+
+}
