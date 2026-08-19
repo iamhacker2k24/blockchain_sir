f9230731e1bab0b796bcc0bc9e822d132ab5430c
Debabrata24 <mr.debabrtapc2006@gmail.com>
2026-08-19 10:55:00 +0530

    rust

--- Rusts/main.rs
+++ Rusts/main.rs
@@ -1,38 +1,47 @@
+// // fn main() {
+// //     // Define the name variable
+// //     // let name = "Alice";
+
+// //     // Print the name using a placeholder
+// //     // println!("Hello, {}!", name);
+// //    let str = String::from("Hello");
+// //     finder (str);
+// //     let a =10;
+// //    let b =40;
+// //    let ans = add_number(a,b);
+// //    println!("{}",ans);
+// //     // println!("{}",str);
+// //     println!("{}",a);
+// // }
+
+// // fn add_number(x:i32 ,y:i32)->i32{
+// //     return  x +y;
+// //     // println!("{}",x+y);
+// // }
+// // fn finder (s:String){
+// //     println!("{}",s);
+
+// //     //drop
+// // }
+
 // fn main() {
-//     // Define the name variable
-//     // let name = "Alice";
+//     let mut str = String::from("Strike is comming");
 
-//     // Print the name using a placeholder
-//     // println!("Hello, {}!", name);
-//    let str = String::from("Hello");
-//     finder (str);
-//     let a =10;
-//    let b =40;
-//    let ans = add_number(a,b);
-//    println!("{}",ans);
-//     // println!("{}",str);
-//     println!("{}",a);
+//     mutable_borrow(&mut str);
+
+//     println!("{}", str);
 // }
 
-// fn add_number(x:i32 ,y:i32)->i32{
-//     return  x +y;
-//     // println!("{}",x+y);
-// }
-// fn finder (s:String){
-//     println!("{}",s);
-
-//     //drop
+// fn immutable_borrow(s: &String) {
+//     println!("{}", s);
 // }
 
+// fn mutable_borrow(s: &mut String) {
+//     s.push_str(" hi");
+//     println!("{}", s);
+// }
 
-fn main (){
-    let str  = String::from ("Strike is commig");
-    // only read me 
-   immutable_borrow(&str);
-println!("{}", str)
-}
-fn immutable_borrow (s:&String){
- println!("{}",s)       
-}
+// // 40
 
-// 40
+//non primitve data int type 
+
