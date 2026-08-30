0747ba8d7a2fe23d92c12e47933ed3390351f528
Debabrata24 <mr.debabrtapc2006@gmail.com>
2026-08-30 12:29:52 +0530

    oth

--- blockchain_yt/Hardhat/hardhat.config.ts
+++ blockchain_yt/Hardhat/hardhat.config.ts
@@ -34,5 +34,9 @@ export default defineConfig({
       url: configVariable("SEPOLIA_RPC_URL"),
       accounts: [configVariable("SEPOLIA_PRIVATE_KEY")],
     },
+    // ropsten:{
+    //   url:"https://mainnet.infura.io/v3/9f415d2509684b18ba15e1e53589f5f4",
+    //   accounts:["0x48a9572f256bdf0ef6d6e6d1bdf559b2a0cec45f6d543d9db64ea87645e70d49"]
+    // }
   },
 });
